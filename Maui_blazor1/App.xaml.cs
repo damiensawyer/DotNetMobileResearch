fd9b5e088679566779d8b79d2d56c1cbac44f0e4
Damien Sawyer <damiensawyer@gmail.com>
2022-11-14 03:00:02 +0000

    init

---
+++ Maui_blazor1/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui_blazor1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Maui_blazor1/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui_blazor1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
